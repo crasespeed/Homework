23855c03ca948c39cfe5c91c0845cf4a658c84d6
crazespeed <craze_speed@mail.ru>
2021-07-04 12:41:09 +0300

    'ДЗ_2'

---
+++ Homework_2.py
@@ -0,0 +1,61 @@
+                    #  №1
+# list = [-100, 100.5, 'False', True, None]
+# for el in list:
+#     print(type(el))
+
+                    #  №2
+# list = int(input("Введите количество элементов: "))
+# num = []
+# i = 0
+# while i < list:
+#     num.append(input("Введите значение: "))
+#     i += 1
+# for el in range(int(len(num)/2)):
+#         num[el], num[el + 1] = num [el + 1], num[el]
+#         el += 2
+# print(num)
+                    #  №3
+# dict = ['Зима', 'Весна', 'Лето', 'Осень']
+# month = int(input("Введите номер месяца "))
+# if  month in [12,1,2,]:
+#      print(dict[0])
+# elif month in [3,4,5,]:
+#     print(dict[1])
+# elif month in [6,7,8,]:
+#     print(dict[2])
+# elif month in [9,10,11,]:
+#     print(dict[3])
+# else:
+#     month > 12
+#     print('В году двеннадцать месяцев!')
+
+                    #  №4
+
+# word = input('Введите несколько слов ')
+# word = str.split(word)
+# for num, el in enumerate(word):
+#     if len(str(word)) <= 10:
+#         print(num, el)
+#     else:
+#         len(str(word)) <= 10
+#         print(num, el [0:10])
+
+                    #  №5
+
+# num = int(input("Введите цифру: "))
+# list = [9, 7, 5, 3, 1 ]
+# a = list.count(num)
+# for el in list:
+#     if a > 0:
+#         b = list.index(num)
+#         list.insert(b+a, num)
+#         break
+#     else:
+#         if num > el:
+#             c = list.index(el)
+#             list.insert(c, num)
+#             break
+#         elif num < list[len(list) - 1]:
+#             list.append(num)
+# print(list)
+
