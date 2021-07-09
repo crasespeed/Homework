9a6b1ac9a4c26e295e6a8f7efc9bca24d6e4c59d
crazespeed <craze_speed@mail.ru>
2021-07-09 15:15:39 +0300

    i

--- HW_2.py
+++ HW_2.py
@@ -1,4 +1,4 @@
 list = [300, 2, 12, 44, 1, 1, 4, 10, 7, 1, 78, 123, 55]
 print(f'Исходный список, {list}')
 my_list = [el for num, el in enumerate(list) if list[num-1] < list[num]]
-print(f'Новый список {my_list (1)}')
+print(f'Новый список {my_list}')
