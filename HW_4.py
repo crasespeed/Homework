09c0db25289b0b49f3086de834e6c7b7328f6981
crazespeed <craze_speed@mail.ru>
2021-07-09 13:08:10 +0300

    i

--- HW_4.py
+++
@@ -1,6 +0,0 @@
-def func():
-    x = float(input("Введите положительное число: "))
-    y = float(input("Введите степень ввиде отрацельного числа: "))
-    res = x ** y
-    return res
-print(f'Рузультат  {func()}')
