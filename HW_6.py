f573c4116375262bc35728227afb7fcbc2305f44
crazespeed <craze_speed@mail.ru>
2021-07-10 15:49:49 +0300

    i

---
+++ HW_6.py
@@ -0,0 +1,13 @@
+from itertools import count, cycle
+
+for el in count(3):
+    if el > 10:
+        break
+    print(el)
+
+a = 0
+for el in cycle('ABCDEFJ'):
+    if a > 10:
+        break
+    print(el)
+    a += 1
