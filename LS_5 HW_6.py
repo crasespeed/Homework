d13918ee34a8522b19cb08b75f676c864a2cdeb0
crazespeed <craze_speed@mail.ru>
2021-07-15 20:30:20 +0300

    HW_

--- LS_5 HW_6.py
+++ LS_5 HW_6.py
@@ -5,13 +5,3 @@ with open('file_6.txt', 'r') as file_6:
         subj[subject] = int(lecture) + int(practice) + int(lab)
     print(f'Общее количество часов по предмету - \n {subj}')
 
-
-# import codecs
-# import re
-# subj = {}
-# with codecs.open('file_6.txt', 'r', 'utf-8') as file_6:
-#     for line in file_6:
-#         subject, lecture, practice, lab = line.split()
-#         my_list = lecture + practice + lab
-#         subj[subject] = sum(map(int, re.findall(r'\d+', my_list)))
-#     print(f'Общее количество часов по предмету - \n {subj}')
