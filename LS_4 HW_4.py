2b41f704766b11908fc43eb0e2ce36d5e11747ea
crazespeed <craze_speed@mail.ru>
2021-07-19 14:42:46 +0300

    HW

--- LS_4 HW_4.py
+++
@@ -1,2 +0,0 @@
-list = [2, 2, 2, 7, 23, 1, 44, 22, 15, 44, 3, 2, 10, 7, 4, 11]
-print([el for el in list if list.count(el) == 1])
--- LS_5 HW_1.py
+++
@@ -1,9 +0,0 @@
-with open('file_1.txt', 'w+') as my_f:
-    text = input('Введите текст: ')
-    while text:
-        my_f.write(f"{text}\n")
-        text = input('Введите текст: ')
-        if not text:
-            break
-    my_f.seek(0)
-    print(my_f.read())
