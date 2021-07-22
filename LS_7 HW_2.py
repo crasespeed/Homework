35bd83391168ec2db2614a2702af9a5c8e3027ee
crazespeed <craze_speed@mail.ru>
2021-07-22 18:38:45 +0300

    HW

---
+++ LS_7 HW_2.py
@@ -0,0 +1,38 @@
+from abc import ABC, abstractmethod
+
+class Clothes(ABC):
+    def __init__(self, param):
+        self.param = param
+
+    @property
+    def max_area(self):
+        return f'Площадь ткани: {(self.param / 6.5 + 0.5) + (2 * self.param + 0.3)}'
+
+    @abstractmethod
+    def abstractmethod(self):
+        pass
+
+class Coat(Clothes):
+    def __init__(self, param):
+        super(Coat, self).__init__(param)
+
+    def max_area(self):
+        return f'Ткани для пальто: {self.param / 6.5 + 0.5:.2f}'
+
+    def abstractmethod(self):
+        pass
+
+class Suit(Clothes):
+    def __init__(self, param):
+        super(Suit, self).__init__(param)
+
+    def max_area(self):
+        return f'Ткани для костюма: {2 * self.param + 0.3:.2f}'
+
+    def abstractmethod(self):
+        pass
+
+coat = Coat(5)
+suit = Suit(3)
+print(coat.max_area())
+print(suit.max_area())
